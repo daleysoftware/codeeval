e5d94b79068a79c5fd993f097c69c8ffd9fefb56
Matt Pillar <matt@aerofs.com>
2013-11-06 18:16:49 -0800

    Detecting cycles solution.
    Not the most elegant, since it's the brute force solution, but it
    captures all the corner cases, whereas with the tortoise and hare
    solution capturing those cases was pretty difficult.

--- moderate/detectingcycles/Main.java
+++ moderate/detectingcycles/Main.java
@@ -22,61 +22,14 @@ public class Main
             String[] lineArray = line.split(" ");
             int[] sequence = new int[lineArray.length];
 
-            //System.out.println(">>> " + line);
             for (int i = 0; i < sequence.length; i++) {
                 sequence[i] = Integer.parseInt(lineArray[i]);
             }
 
-            // Need to remove duplicates, since the tortoise and the hare algorithm does
-            // not handle them properly.
-            int[] noDuplicatesSequence = new int[sequence.length];
-            int[] elementFrequency = new int[sequence.length];
-
-            int currentElement = 0;
-
-            noDuplicatesSequence[0] = sequence[0];
-            elementFrequency[0] = 1;
-
-            for (int i = 1; i < sequence.length; i++) {
-                int current = sequence[i];
-
-                if (current == noDuplicatesSequence[currentElement]) {
-                    elementFrequency[currentElement]++;
-                } else {
-                    currentElement++;
-                    noDuplicatesSequence[currentElement] = current;
-                    elementFrequency[currentElement] = 1;
-                }
-            }
-
-            /*for (int i = 0; i < noDuplicatesSequence.length; i++) {
-                System.out.println(noDuplicatesSequence[i] + " " + elementFrequency[i]);
-            }*/
-
-            CycleInformation ci = detectFirstCycle(
-                    noDuplicatesSequence,
-                    currentElement+1);
-
-            if (ci == null) {
-                // No repeating sequence. It must be the corner case where the last digit
-                // is repeated.
-                System.out.println(noDuplicatesSequence[currentElement]);
-                continue;
-            }
+            CycleInformation ci = detectFirstCycle(sequence);
 
             for (int i = 0; i < ci.cycleLength(); i++) {
-                int index = ci.startingPoint() + i;
-
-                int current = noDuplicatesSequence[index];
-                int frequency = elementFrequency[index];
-
-                for (int j = 0; j < frequency; j++) {
-                    System.out.print(current);
-
-                    if (j != frequency - 1) {
-                        System.out.print(" ");
-                    }
-                }
+                System.out.print(sequence[ci.startingPoint() + i]);
 
                 if (i != ci.cycleLength() - 1) {
                     System.out.print(" ");
@@ -91,66 +44,56 @@ public class Main
 
     /**
      * Detect the first cycle in the given sequence.
-     *
-     * See Wikipedia's description of the tortoise and the hare algorithm:
-     * http://en.wikipedia.org/wiki/Floyd%27s_cycle-finding_algorithm#Tortoise_and_hare
-     *
-     * N.B. cannot have any repeated elements in the sequence.
-     *
-     * @return A pair containing two integers: mu and lambda. Mu is the starting position
-     * of the cycle and lambda is the length of the cycle.
      */
-    public static CycleInformation detectFirstCycle(int[] sequence, int length)
+    public static CycleInformation detectFirstCycle(int[] sequence)
     {
-        // Dump the sequence into a hash map to serve as the function f().
-        HashMap<Integer, Integer> f = new HashMap<Integer, Integer>();
-
-        for (int i = 1; i < length; i++) {
-            f.put(sequence[i-1], sequence[i]);
-        }
-
-        // Standard tortoise and hare algorithm follows.
-        int tortoise = f.get(sequence[0]);
-        if (!f.containsKey(tortoise)) {
-            return null;
-        }
-        int hare = f.get(tortoise);
-
-        while (tortoise != hare) {
-            tortoise = f.get(tortoise);
-
-            // Watch for the case when we reach the end of the sequence and we haven't
-            // found a repeating pattern.
-            if (!f.containsKey(hare)) {
-                return null;
-            } else {
-                hare = f.get(hare);
-
-                if (!f.containsKey(hare)) {
-                    return null;
+        for (int starting = 0; starting < sequence.length-1; starting++) {
+            //System.out.println("starting: " + starting);
+            for (int length = 1; length < sequence.length; length++) {
+                //System.out.println("length: " + length);
+                if (isValidCycle(sequence, starting, length)) {
+                    return new CycleInformation(starting, length);
                 }
-
-                hare = f.get(hare);
             }
         }
 
-        int mu = 0;
-        tortoise = sequence[0];
-        while (tortoise != hare) {
-            tortoise = f.get(tortoise);
-            hare = f.get(hare);
-            mu++;
+        return new CycleInformation(0, sequence.length);
+    }
+
+    private static boolean isValidCycle(int[] sequence, int starting, int length)
+    {
+        int current = starting + length;
+        boolean foundMatch = false;
+
+        while (current < sequence.length) {
+            if (!subArraysMatch(sequence, starting, current, length)) {
+                return false;
+            }
+
+            foundMatch = true;
+            current += length;
         }
 
-        int lambda = 1;
-        hare = f.get(tortoise);
-        while (tortoise != hare) {
-            hare = f.get(hare);
-            lambda++;
+        return foundMatch;
+    }
+
+    private static boolean subArraysMatch(int[] sequence, int startingA, int startingB,
+                                          int length)
+    {
+        for (int i = 0; i < length; i++) {
+            int indexA = startingA + i;
+            int indexB = startingB + i;
+
+            if (indexA >= sequence.length || indexB >= sequence.length) {
+                return false;
+            }
+
+            if (sequence[indexA] != sequence[indexB]) {
+                return false;
+            }
         }
 
-        // starting point, length == mu, lambda.
-        return new CycleInformation(mu, lambda);
+        return true;
     }
 
     /**
