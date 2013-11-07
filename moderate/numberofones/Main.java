40b81644c12d697d11fee6c3d09f690c801d1eeb
Matt Pillar <matt@aerofs.com>
2013-11-06 21:21:27 -0800

    Number of ones solution.

---
+++ moderate/numberofones/Main.java
@@ -0,0 +1,47 @@
+package codeeval.moderate.numberofones;
+
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main (String[] args)
+            throws IOException
+    {
+        File file = new File(args[0]);
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            line = line.trim();
+
+            if (line.length() == 0) {
+                continue;
+            }
+
+            int n = Integer.parseInt(line);
+            int numberOfOnes = countNumberOfOneBits(n);
+
+            System.out.println(numberOfOnes);
+        }
+
+        in.close();
+    }
+
+    public static int countNumberOfOneBits(int n)
+    {
+        int count = 0;
+
+        while (n != 0) {
+            int lastBit = n & 0x1;
+
+            if (lastBit != 0) {
+                count++;
+            }
+
+            n >>= 1;
+        }
+
+        return count;
+    }
+}
