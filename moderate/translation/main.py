a94e422d70fd37e5f27f2f29c2f5a06773e995fe
Matt Pillar <matt@aerofs.com>
2014-03-13 14:00:49 -0700

    Solution to the translation problem (in python).

--- moderate/translation/main.py
+++ moderate/translation/main.py
@@ -1,12 +1,39 @@
 import sys
 
-# TODO finish
+# Determined by the input/output samples.
 mapping = {
-    'r': 't',
+    'a': 'y',
     'b': 'h',
     'c': 'e',
-    'v': 'p'
-}
+    'd': 's',
+    'e': 'o',
+    'f': 'c',
+    'g': 'v', # guess
+    'h': 'x', # guess
+    'i': 'd',
+    'j': 'u',
+    'k': 'i',
+    'l': 'g',
+    'm': 'l',
+    'n': 'b',
+    'o': 'k',
+    'p': 'r',
+    'q': 'z',
+    'r': 't',
+    's': 'n',
+    't': 'w',
+    'u': 'j',
+    'v': 'p',
+    'w': 'f',
+    'x': 'm',
+    'y': 'a',
+    'z': 'q'}
+
+def get_mapping(char):
+    if char == ' ':
+        return ' '
+    else:
+        return mapping[char]
 
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
@@ -14,6 +41,10 @@ for test in test_cases:
     if len(test) == 0:
         continue
 
-    print test
+    output = ""
+    for i in range(0, len(test)):
+        output += get_mapping(test[i])
+
+    print output
 
 test_cases.close()
