9727d00c051004f69053ca22fe6399bbdeb687cf
Matt Pillar <matt@aerofs.com>
2014-03-26 15:47:23 -0700

    First crack at the word chain problem.

--- moderate/wordchain/main.py
+++ moderate/wordchain/main.py
@@ -1,21 +1,10 @@
 import sys
 import itertools
 
-test_cases = open(sys.argv[1], 'r')
-for test in test_cases:
-    test = test.strip()
-    if len(test) == 0:
-        continue
+# TODO (MP) need to improve this -- it's too slow.
+def find_longest_chain_length_directed_maybe_cyclic_graph(graph):
+    permutations = itertools.permutations(graph)
 
-    first_and_last = []
-    for word in test.split(','):
-        first_and_last.append(word[0] + word[-1])
-
-    # Could probably be smarter about how we find the longest path in a graph,
-    # but for now we'll try the permutation approach and see how it performs.
-    #
-    # ... Turns out this is too slow and will need to be revised.
-    permutations = itertools.permutations(first_and_last)
     longest_chain_length = 0
     for p in permutations:
         chain_length = 0
@@ -31,6 +20,24 @@ for test in test_cases:
             longest_chain_length = chain_length
 
     result = 0 if longest_chain_length == 0 else longest_chain_length + 1
-    print result
+    return result
+
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    test = test.strip()
+    if len(test) == 0:
+        continue
+
+    graph = []
+    for word in test.split(','):
+        graph.append(word[0] + word[-1])
+
+    result = find_longest_chain_length_directed_maybe_cyclic_graph(graph)
+
+    if result == 0:
+        print 'None'
+    else:
+        print result
 
 test_cases.close()
