4dfa3a3cebf715272485254ebd6f607d6ffab7ab
Matt Pillar <matt@aerofs.com>
2014-03-30 18:16:38 -0700

    Solution to the prefix expression problem.

---
+++ hard/prefixexpr/main.py
@@ -0,0 +1,28 @@
+import sys
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    test = test.strip()
+    if len(test) == 0:
+        continue
+
+    stack = []
+    for i in reversed(test.split(' ')):
+        if i.isdigit():
+            stack.append(i)
+        else:
+            op1 = float(stack.pop())
+            op2 = float(stack.pop())
+
+            if i == '+':
+                result = op1 + op2
+            elif i == '*':
+                result = op1 * op2
+            else:
+                result = op1 / op2
+
+            stack.append(result)
+
+    print int(stack.pop())
+
+test_cases.close()
