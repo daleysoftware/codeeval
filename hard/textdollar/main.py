4755ba085ad44a0f2221b5852febe2f0e4acf29d
Matt Pillar <matt@aerofs.com>
2014-04-15 10:39:24 -0700

    Solution to the text dollar problem.

---
+++ hard/textdollar/main.py
@@ -0,0 +1,70 @@
+import sys
+
+def number_to_text(n):
+    if n < 20:
+        return {
+            1: "One",
+            2: "Two",
+            3: "Three",
+            4: "Four",
+            5: "Five",
+            6: "Six",
+            7: "Seven",
+            8: "Eight",
+            9: "Nine",
+            10: "Ten",
+            11: "Eleven",
+            12: "Twelve",
+            13: "Thirteen",
+            14: "Fourteen",
+            15: "Fifteen",
+            16: "Sixteen",
+            17: "Seventeen",
+            18: "Eighteen",
+            19: "Nineteen"
+        }[n]
+    elif n < 100:
+        result = {
+            20: "Twenty",
+            30: "Thirty",
+            40: "Forty",
+            50: "Fifty",
+            60: "Sixty",
+            70: "Seventy",
+            80: "Eighty",
+            90: "Ninety"
+        }[(n/10)*10]
+        if n % 10 != 0:
+            result += number_to_text(n % 10)
+        return result
+    elif n < 1000:
+        result = number_to_text(n/100) + "Hundred"
+        if n % 100 > 0:
+            result += number_to_text(n % 100)
+        return result
+    elif n < 1000000:
+        result = number_to_text(n/1000) + "Thousand"
+        if n % 1000 > 0:
+            result += number_to_text(n % 1000)
+        return result
+    elif n < 10**9:
+        result = number_to_text(n/10**6) + "Million"
+        if n % 10**6 > 0:
+            result += number_to_text(n % 10**6)
+        return result
+    else:
+        result = number_to_text(n/10**9) + "Billion"
+        if n % 10**9 > 0:
+            result += number_to_text(n % 10**9)
+        return result
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    test = test.strip()
+    if len(test) == 0:
+        continue
+
+    n = int(test)
+    print number_to_text(n) + "Dollars"
+
+test_cases.close()
