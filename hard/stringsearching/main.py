4e9291fecd7a870ac5596928b2a4c1ef7f3ff911
Matt Pillar <matt@aerofs.com>
2014-04-16 12:09:03 -0700

    Solution to the string searching problem.

--- hard/stringsearching/main.py
+++ hard/stringsearching/main.py
@@ -1,41 +1,17 @@
 import sys
 
 def search(to_search, pattern):
-    for i in range(len(to_search)):
+    to_search = to_search.replace("*", "#")
+    pattern = pattern.replace("\\*", "#")
 
-        to_search_index = i
-        pattern_index = 0
+    for p in pattern.split("*"):
+        found_index = to_search.find(p)
+        if found_index < 0:
+            return False
 
-        star = False
-        pattern_char_prev = ''
+        to_search = to_search[found_index+len(p):]
 
-        while True:
-            if to_search_index == len(to_search) or pattern_index == len(pattern):
-                break
-
-            to_search_char = to_search[to_search_index]
-            pattern_char = pattern[pattern_index]
-
-            if pattern_char == '*' and pattern_char_prev != '\\':
-                star = True
-                pattern_index += 1
-
-            if to_search_char == pattern_char:
-                to_search_index += 1
-                pattern_index += 1
-
-                if pattern_index == len(pattern):
-                    return True
-
-                star = False
-            else:
-                if star:
-                    to_search_index += 1
-                    continue
-                else:
-                    break
-
-    return False
+    return True
 
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
