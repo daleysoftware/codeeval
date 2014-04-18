2ca078f3dbd0566066d37fd53b4dc7c52be4adb9
Matt Pillar <matt@aerofs.com>
2014-04-18 10:27:02 -0700

    Solution to the decryption problem.

---
+++ 2-hard/decryption/main.py
@@ -0,0 +1,19 @@
+def main():
+    message = '012222  1114142503  0313012513  03141418192102  0113  2419182119021713  06131715070119'
+    key = 'BHISOECRTMGWYVALUZDNFJKPQX'
+
+    count = 0
+    key_map = {}
+    for c in key:
+        key_map[c] = count
+        count += 1
+
+    result = ""
+    for i in xrange(0, len(message), 2):
+        if message[i:i+2] == '  ': result += ' '; continue
+        result += chr(ord('A') + key_map[chr(ord('A') + int(message[i:i+2]))])
+
+    print result
+
+if __name__ == '__main__':
+    main()
