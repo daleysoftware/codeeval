9e0e6c725fe63a46959b8047f91138bef8e7368e
Matt Pillar <matt@aerofs.com>
2014-04-18 15:54:50 -0700

    Port robot movement problem to C++.

--- 2-hard/robot-movements/main.cpp
+++ 2-hard/robot-movements/main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 
-bool grid[4][4] = {
-    {false, false, false, false},
-    {false, false, false, false},
-    {false, false, false, false},
-    {false, false, false, false}
-};
+int countPaths(bool grid[][4], int currentX, int currentY)
+{
+    if (currentX < 0 || currentX > 3 || currentY < 0 || currentY > 3) {
+        return 0;
+    }
+
+    if (grid[currentX][currentY]) {
+        return 0;
+    }
+
+    if (currentX == 3 && currentY == 3) {
+        return 1;
+    }
+
+    grid[currentX][currentY] = true;
+
+    int result = 0;
+
+    result += countPaths(grid, currentX+1, currentY);
+    result += countPaths(grid, currentX,   currentY+1);
+    result += countPaths(grid, currentX-1, currentY);
+    result += countPaths(grid, currentX,   currentY-1);
+
+    grid[currentX][currentY] = false;
+
+    return result;
+}
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
-    // TODO
+    bool grid[][4] = {
+        {false, false, false, false},
+        {false, false, false, false},
+        {false, false, false, false},
+        {false, false, false, false}
+    };
+
+    std::cout << countPaths(grid, 0, 0) << std::endl;
     return 0;
 }
