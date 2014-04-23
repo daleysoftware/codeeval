abee435bf9d73af5c553aa538befa400e0e7177b
Matt Pillar <matt@aerofs.com>
2014-04-22 17:01:42 -0700

    Two more ports. Endianness to C++ and remove chars to python.

---
+++ 1-moderate/endianness/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+enum
+{
+    O32_LITTLE_ENDIAN = 0x03020100ul,
+    O32_BIG_ENDIAN = 0x00010203ul
+};
+
+int main()
+{
+    static const union {
+        unsigned char bytes[4];
+        uint32_t value;
+    } o32_host_order = { { 0, 1, 2, 3 } };
+
+    if (o32_host_order.value == O32_LITTLE_ENDIAN) {
+        std::cout << "LittleEndian" << std::endl;
+    } else {
+        std::cout << "BigEndian" << std::endl;
+    }
+}
---
+++ 1-moderate/remove-characters/main.py
@@ -0,0 +1,18 @@
+import sys
+
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    test = test.strip()
+    if len(test) == 0:
+        continue
+
+    words = test.split(',')[0].strip()
+    chars = test.split(',')[1].strip()
+
+    for c in chars:
+        words = words.replace(c, '')
+
+    print words
+
+test_cases.close()
