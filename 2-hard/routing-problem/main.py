52f86173269147989cdd53003584279efd50a91d
Matt Pillar <matt@aerofs.com>
2014-04-24 16:38:19 -0700

    Another iteration of the routing problem question. Still broken.

--- 2-hard/routing-problem/main.py
+++ 2-hard/routing-problem/main.py
@@ -16,90 +16,72 @@ class Graph:
         self.edges[from_node].append(to_node)
         self.edges[to_node].append(from_node)
 
-# TODO too slow
-def find_all_paths_using_bfs(graph, current, end, prefix=None, visited=set()):
-    if prefix is None:
-        prefix = []
+# TODO broken, needs fixing.
+def find_all_paths(graph, start, end):
+    queue = [start]
+    paths = collections.defaultdict(list)
+    distances = collections.defaultdict(int)
+    visited = set()
 
-    if current == end:
-        return [prefix + [end]]
-    if current in visited:
-        return []
+    while len(queue) > 0:
+        node = queue.pop()
+        new_paths = [p + [node] for p in paths[node]]
+        if len(new_paths) == 0:
+            new_paths = [[node]]
+        min_distance = len(min(new_paths, key=len))
 
-    visited.add(current)
-    result_paths = []
+        for next_node in graph.edges[node]:
+            next_node_visited = (node, next_node) in visited
+            visited.add((node, next_node))
 
-    for node in graph.edges[current]:
-        paths = find_all_paths_using_bfs(graph, node, end, prefix + [current], visited)
+            if not next_node_visited:
+                if distances[next_node] == 0 or min_distance+1 <= distances[next_node]:
+                    # TODO can improve this by trimming out longer paths at this step.
+                    paths[next_node].extend(new_paths)
+                    distances[next_node] = len(min(paths[next_node], key=len))
+                    if next_node not in queue: queue.append(next_node)
 
-        if len(paths) > 0:
-            result_paths.extend(paths)
-
-    visited.remove(current)
-    return result_paths
-
-# TODO too slow
-def find_all_paths_using_queue(graph, start, end):
-    path  = []
-    paths = []
-    queue = [(start, end, path)]
-    while queue:
-        start, end, path = queue.pop()
-        path = path + [start]
-        if start == end:
-            paths.append(path)
-        for node in set(graph.edges[start]).difference(path):
-            queue.append((node, end, path))
-    return paths
+    return [p + [end] for p in paths[end]]
 
 def make_mask(n):
     result = 0xffffffff
-    for i in range(32-int(n)): result -= (1 << i)
+    for i in xrange(32-int(n)): result -= (1 << i)
     return result
 
 def dotted_quad_to_num(ip):
     return struct.unpack('!I',socket.inet_aton(ip))[0]
 
-def network_mask(ip, bits):
-    return dotted_quad_to_num(ip) & make_mask(bits)
-
-def node1_can_reach_node2(n1, n2, node_to_ip_mapping):
-    ips1 = node_to_ip_mapping[n1]
-    ips2 = node_to_ip_mapping[n2]
-
-    for ip1 in ips1:
-        nm1 = make_mask(ip1.split('/')[1])
-        ip1 = dotted_quad_to_num(ip1.split('/')[0])
-        for ip2 in ips2:
-            nm2 = make_mask(ip2.split('/')[1])
-            ip2 = dotted_quad_to_num(ip2.split('/')[0])
-
-            if ip1 & nm1 == ip2 & nm2:
+def node1_can_reach_node2(n1, n2, node_to_ips_mapping):
+    for ip1 in node_to_ips_mapping[n1]:
+        for ip2 in node_to_ips_mapping[n2]:
+            if ip1[0] & ip1[1] == ip2[0] & ip2[1]:
                 return True
-
     return False
 
 def main():
     test_cases = open(sys.argv[1], 'r')
 
     graph_text = test_cases.readline()
-    node_to_ip_mapping = ast.literal_eval(graph_text)
+    node_to_ips_mapping = ast.literal_eval(graph_text)
     graph = Graph()
+    nodes = node_to_ips_mapping.keys()
+    for node in nodes:
+        ips = node_to_ips_mapping[node]
+        new_ips = []
+        for ip in ips:
+            new_ips.append([dotted_quad_to_num(ip.split('/')[0]),
+                           make_mask(ip.split('/')[1])])
+        node_to_ips_mapping[node] = new_ips
 
-    nodes = node_to_ip_mapping.keys()
-
-    for i in range(len(nodes)):
+    for i in xrange(len(nodes)):
         node1 = nodes[i]
         graph.add_node(node1)
-
-        for j in range(i, len(nodes)):
+        for j in xrange(i, len(nodes)):
             node2 = nodes[j]
             graph.add_node(node2)
-
             if node1 == node2:
                 continue
-
-            if node1_can_reach_node2(node1, node2, node_to_ip_mapping):
+            if node1_can_reach_node2(node1, node2, node_to_ips_mapping):
                 graph.add_edge(node1, node2)
 
     for test in test_cases:
@@ -109,20 +91,16 @@ def main():
 
         start = int(test.split(' ')[0])
         end = int(test.split(' ')[1])
-
-        paths = sorted(find_all_paths_using_queue(graph, start, end), key=len)
+        paths = sorted(find_all_paths(graph, start, end), key=len)
         if len(paths) == 0:
             print 'No connection'
             continue
-
         best_length = len(paths[0])
         best_paths = []
         for path in paths:
             if len(path) != best_length:
                 break
-
             best_paths.append(str(path))
-
         print ", ".join(best_paths)
 
     test_cases.close()
