967cf9d8e6c5489426b431225e5f448da8c94b75
Matt Pillar <matt@aerofs.com>
2014-07-23 15:34:12 -0700

    First pass at Locks problem.
    Only getting partial completion at this point; buggy.

---
+++ 1-moderate/locks/main.py
@@ -0,0 +1,34 @@
+import sys
+
+def do_lock_pass(locked):
+    for i in xrange(0, len(locked), 2):
+        locked[i] = True
+
+def do_flip_pass(locked):
+    for i in xrange(0, len(locked), 3):
+        locked[i] = not locked[i]
+
+def count_unlocked(locked):
+    result = 0
+    for l in locked:
+        if not l: result += 1
+    return result
+
+def main():
+    test_cases = open(sys.argv[1], 'r')
+    for test in test_cases:
+        test = test.strip()
+        num_locks = int(test.split(' ')[0])
+        num_iterations = int(test.split(' ')[1])
+        locked = [False] * num_locks
+
+        for i in xrange(num_iterations-1):
+            do_lock_pass(locked)
+            do_flip_pass(locked)
+
+        locked[-1] = True
+        print count_unlocked(locked)
+
+    test_cases.close()
+
+main()
