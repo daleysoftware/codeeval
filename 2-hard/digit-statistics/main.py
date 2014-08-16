b75327665d31a8b182a8232fcfe0d233f39b0d18
Matt Pillar <matt@aerofs.com>
2014-08-16 15:18:47 -0700

    Solution to the digit statistics problem.

--- 2-hard/digit-statistics/main.py
+++ 2-hard/digit-statistics/main.py
@@ -1,28 +1,44 @@
 import sys
+import collections
 
-test_cases = open(sys.argv[1], 'r')
-for test in test_cases:
-    test = test.strip()
-    if len(test) == 0:
-        continue
+# This is the trick: take advantage of the multiplicative pattern.
+def get_cycle(number):
+    cycle = []
+    current = number
+    while current not in cycle:
+        cycle.append(current)
+        current = (current * number) % 10
+    return cycle
 
-    a = int(test.split(' ')[0])
-    n = int(test.split(' ')[1])
-    an = str(a**n)
+def get_digits(a, n):
+    cycle = get_cycle(a)
+    digits = collections.defaultdict(int)
+    cycle_repeats = n/len(cycle)
+    remaining = n - cycle_repeats * len(cycle)
+    for c in cycle:
+        digits[c] = cycle_repeats
+    for i in xrange(1, remaining+1):
+        digit = cycle[(i-1)%len(cycle)]
+        digits[digit] += 1
+    return digits
 
-    digits = {}
-    for c in an:
-        c = int(c)
-        if c in digits:
-            digits[c] += 1
-        else:
-            digits[c] = 1
-
+def print_pretty_default_dict(d):
     result = []
-    for i in range(10):
-        x = 0 if i not in digits else digits[i]
-        result.append("%i: %i" % (i, x))
+    for i in xrange(0, 10):
+        result.append("%i: %i" % (i, d[i]))
+    print ', '.join(result)
 
-    print ", ".join(result)
+def main():
+    test_cases = open(sys.argv[1], 'r')
+    for test in test_cases:
+        test = test.strip()
+        if len(test) == 0:
+            continue
+        a = int(test.split(' ')[0])
+        n = int(test.split(' ')[1])
+        digits = get_digits(a, n)
+        print_pretty_default_dict(digits)
+    test_cases.close()
 
-test_cases.close()
+if __name__ == '__main__':
+    main()
