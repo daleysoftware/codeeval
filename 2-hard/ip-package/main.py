c9fafdbbd4b76304eb6a20c500df7bd9d915cb0c
Matt Pillar <matt@aerofs.com>
2014-08-21 13:15:56 -0700

    Makefile improvements and IP package shell.

---
+++ 2-hard/ip-package/main.py
@@ -0,0 +1,12 @@
+import sys
+import collections
+
+def main():
+    indexes = {}
+    with open(sys.argv[1], 'r') as test_cases:
+        for test in test_cases:
+            # TODO finish this.
+            print test.strip()
+
+if __name__ == '__main__':
+    main()
--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ all:
 	@cd 0-easy && make
 	@cd 1-moderate && make
 	@cd 2-hard && make
+
+clean:
+	@find . -type f -name "a.out" -exec rm -f {} \;
