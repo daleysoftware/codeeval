890266422436a0d626f8c00812d5e5313901aa81
Matt Pillar <matt@aerofs.com>
2014-12-29 22:49:40 -0800

    WIP: the cubes problem. Added traveling logic.

--- 2-hard/the-cubes/main.py
+++ 2-hard/the-cubes/main.py
@@ -1,4 +1,25 @@
 import sys
+from enum import Enum
+
+class Element(Enum):
+    WALL = 1
+    HOLE = 2
+    FLOOR = 3
+
+CHAR_TO_ELEMENT_ENUM = {
+    '*': Element.WALL,
+    'o': Element.HOLE,
+    ' ': Element.FLOOR
+}
+
+class Point(object):
+    def __init__(self, level, row, col):
+        self.level = level
+        self.row = row
+        self.col = col
+
+    def adjacent_to(self, point):
+        return abs(self.level-point.level) + abs(self.row-point.row) + abs(self.col-point.col) == 1
 
 class Cube(object):
     def __init__(self, levels):
@@ -20,13 +41,13 @@ class Cube(object):
 
     def _find_empty_cell_in_border(self, level):
         # Top row.
-        for i in xrange(0, self.length):
+        for i in range(0, self.length):
             if self.levels[level][0][i] == ' ': return 0, i
         # Bottom row.
-        for i in xrange(0, self.length):
+        for i in range(0, self.length):
             if self.levels[level][self.length-1][i] == ' ': return self.length-1, i
         # Sides.
-        for i in xrange(1, self.length-1):
+        for i in range(1, self.length-1):
             if self.levels[level][i][0] == ' ': return i, 0
             if self.levels[level][i][self.length-1] == ' ': return i, self.length-1
         # Should never reach here if there is an empty cell in the border.
@@ -40,14 +61,38 @@ class Cube(object):
     def exit(self):
         return self._find_empty_cell_in_border(self.length-1)
 
-    def element(self, level, row, col):
-        if level < 0 or row < 0 or col < 0 or level >= self.length or row >= self.length or col >= self.length:
-            return '*'
-        return self.levels[level][row][col]
+    def _element(self, point):
+        if point.level < 0 or point.row < 0 or point.col < 0 or \
+           point.level >= self.length or point.row >= self.length or point.col >= self.length:
+            return Element.WALL
+        return CHAR_TO_ELEMENT_ENUM[self.levels[point.level][point.row][point.col]]
+
+    def _can_travel(self, point_from, point_to):
+        # Precondition.
+        if self._element(point_from) == Element.WALL or self._element(point_to) == Element.WALL:
+            return False
+
+        # Adjacency.
+        if not point_from.adjacent_to(point_to):
+            return False
+
+        # Various semantic cases, depending on levels.
+        if point_from.level == point_to.level:
+            return True
+        elif point_from.level == point_to.level + 1:
+            # "from" is one level above "to", i.e. we are moving down.
+            return self._element(point_from) == Element.HOLE
+        elif point_from.level + 1 == point_to.level:
+            # "from" is one level below "to", i.e. we are moving up.
+            return self._element(point_to) == Element.HOLE
+        else:
+            return False
 
     @property
     def min_steps(self):
         # TODO finish this...
+        # Need to use some sort of data structure to track where we have visited... either a 3D
+        # matrix, or perhaps some sort of map (with a default value).
         return 0
 
 def main():
@@ -58,15 +103,15 @@ def main():
             continue
 
         length = int(test.split(';')[0])
-        levels = zip(*[iter(zip(*[iter(test.split(';')[1])]*length))]*length)
+        levels = list(zip(*[iter(zip(*[iter(test.split(';')[1])]*length))]*length))
 
         cube = Cube(levels)
 
         # TODO remove extra print statements.
-        print "============ %i" % cube.length
-        print cube.entrance
-        print cube.exit
-        print cube.min_steps
+        print("============ %i" % cube.length)
+        print(cube.entrance)
+        print(cube.exit)
+        print(cube.min_steps)
 
     test_cases.close()
 
