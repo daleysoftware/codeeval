771b7a7b900b69112bd4ab51be736f8e7cb27d2b
Matt Pillar <matt@aerofs.com>
2015-01-05 17:02:50 -0800

    Formatting.

--- 1-moderate/guess-the-number/main.py
+++ 1-moderate/guess-the-number/main.py
@@ -6,17 +6,17 @@ class NumberGuesser:
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
-    def getGuess(self):
+    def get_guess(self):
         return int(math.ceil((self.lower_bound + self.upper_bound) / 2.0))
 
-    def hasSolution(self):
+    def has_solutions(self):
         return self.lower_bound == self.upper_bound
 
     def lower(self):
-        self.upper_bound = self.getGuess() - 1
+        self.upper_bound = self.get_guess() - 1
 
     def higher(self):
-        self.lower_bound = self.getGuess() + 1
+        self.lower_bound = self.get_guess() + 1
 
 def main():
     test_cases = open(sys.argv[1], 'r')
@@ -27,10 +27,10 @@ def main():
         number_guesser = NumberGuesser(0, upper_bound)
         guesses = test.split(' ')[1:-1]
         for guess in guesses:
-            if number_guesser.hasSolution(): raise Exception("solution too early")
+            if number_guesser.has_solutions(): raise Exception("solution too early")
             if guess == "Lower": number_guesser.lower()
             else: number_guesser.higher()
-        print number_guesser.getGuess()
+        print number_guesser.get_guess()
 
     test_cases.close()
 
