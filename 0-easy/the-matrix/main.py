76e4b0b425b8634afa85e2a77fbda2a1f210674a
Matt Pillar <matt@aerofs.com>
2015-01-09 16:37:46 -0800

    Solution to the matrix problem.

---
+++ 0-easy/the-matrix/main.py
@@ -0,0 +1,20 @@
+import sys
+import math
+
+# N.B. there are lots of good solutions to this problem available on
+# http://stackoverflow.com/questions/42519/how-do-you-rotate-a-two-dimensional-array
+
+def main():
+    test_cases = open(sys.argv[1], 'r')
+    for test in test_cases:
+        test = test.strip()
+        if len(test) == 0: continue
+        test = test.split(' ')
+        n = int(math.sqrt(len(test)))
+        matrix = [test[i:i+n] for i in range(0, len(test), n)]
+        rotated = zip(*matrix[::-1])
+        print ' '.join([' '.join(row) for row in rotated])
+    test_cases.close()
+
+if __name__ == '__main__':
+    main()
