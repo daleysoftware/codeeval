8923a1c99e4b999451797edf0cc0e30b50c3ae90
Matt Pillar <matt@aerofs.com>
2015-01-24 20:08:38 -0800

    Remove characters problem: formatting.

--- 1-moderate/remove-characters/main.py
+++ 1-moderate/remove-characters/main.py
@@ -1,18 +1,17 @@
 import sys
 
+def main():
+    test_cases = open(sys.argv[1], 'r')
+    for test in test_cases:
+        test = test.strip()
+        if len(test) == 0:
+            continue
+        words = test.split(',')[0].strip()
+        chars = test.split(',')[1].strip()
+        for c in chars:
+            words = words.replace(c, '')
+        print words
+    test_cases.close()
 
-test_cases = open(sys.argv[1], 'r')
-for test in test_cases:
-    test = test.strip()
-    if len(test) == 0:
-        continue
-
-    words = test.split(',')[0].strip()
-    chars = test.split(',')[1].strip()
-
-    for c in chars:
-        words = words.replace(c, '')
-
-    print words
-
-test_cases.close()
+if __name__ == '__main__':
+    main()
