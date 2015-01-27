a3cc8c249b4b29ab5394e08896c115d8e44b0338
Matt Pillar <matt@aerofs.com>
2015-01-26 21:07:22 -0800

    Formatting.

--- 0-easy/age-distribution/main.py
+++ 0-easy/age-distribution/main.py
@@ -1,32 +1,34 @@
 import sys
 
-test_cases = open(sys.argv[1], 'r')
-for test in test_cases:
-    test = test.strip()
-    if len(test) == 0:
-        continue
+def main():
+    test_cases = open(sys.argv[1], 'r')
+    for test in test_cases:
+        test = test.strip()
+        if len(test) == 0: continue
+        age = int(test)
 
-    age = int(test)
+        if age < 0 or age > 100:
+            print "This program is for humans"
+            continue
 
-    if age < 0 or age > 100:
-        print "This program is for humans"
-        continue
+        if age <= 2:
+            print "Still in Mama's arms"
+        elif age <= 4:
+            print "Preschool Maniac"
+        elif age <= 11:
+            print "Elementary school"
+        elif age <= 14:
+            print "Middle school"
+        elif age <= 18:
+            print "High school"
+        elif age <= 22:
+            print "College"
+        elif age <= 65:
+            print "Working for the man"
+        else:
+            print "The Golden Years"
 
-    if age <= 2:
-        print "Still in Mama's arms"
-    elif age <= 4:
-        print "Preschool Maniac"
-    elif age <= 11:
-        print "Elementary school"
-    elif age <= 14:
-        print "Middle school"
-    elif age <= 18:
-        print "High school"
-    elif age <= 22:
-        print "College"
-    elif age <= 65:
-        print "Working for the man"
-    else:
-        print "The Golden Years"
+    test_cases.close()
 
-test_cases.close()
+if __name__ == '__main__':
+    main()
