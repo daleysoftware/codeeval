b1d0a00a408ceb8338c45e2331ee095b0af105d4
Matt Pillar <matt@aerofs.com>
2015-02-16 18:24:26 -0800

    Solution to the details problem.

---
+++ 0-easy/details/main.py
@@ -0,0 +1,27 @@
+import sys
+
+def main():
+    test_cases = open(sys.argv[1], 'r')
+    for test in test_cases:
+        test = test.strip()
+        if len(test) == 0:
+            continue
+        matrix = test.split(',')
+        iterations = 0
+        while True:
+            finished = False
+            for col in xrange(len(matrix)):
+                row = matrix[col]
+                if '.Y' in row and 'XY' not in row:
+                    matrix[col] = row.replace('.Y', 'Y')
+                else:
+                    finished = True
+                    break
+            if finished:
+                break
+            iterations += 1
+        print iterations
+    test_cases.close()
+
+if __name__ == '__main__':
+    main()
