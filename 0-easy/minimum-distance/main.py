e1367b82ffeef54ea45f11a032a4dd25b25f7de9
Matt Pillar <matt@aerofs.com>
2015-05-08 13:27:56 -0700

    Solution to the minimum distance problem.

---
+++ 0-easy/minimum-distance/main.py
@@ -0,0 +1,22 @@
+import sys
+
+def compute_difference(houses, house):
+    difference = 0
+    for h in houses:
+        difference += abs(h - house)
+    return difference
+
+def main(input_file):
+    with open(input_file, 'r') as fh:
+        for line in fh:
+            houses = sorted([int(x) for x in line.split(' ')[1:]])
+            difference = sys.maxint
+            for house in houses:
+                difference = min(difference, compute_difference(houses, house))
+            print difference
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: %s <input_file>" % sys.argv[0])
+        sys.exit(1)
+    main(sys.argv[1])
