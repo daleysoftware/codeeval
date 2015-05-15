80844de3adde7fa435d1481b80610e0fe8d9e216
Matt Pillar <matt@aerofs.com>
2015-05-15 11:06:00 -0700

    Solution to the swap numbers problem.

---
+++ 0-easy/swap-numbers/main.py
@@ -0,0 +1,13 @@
+import sys
+
+def swap_numbers(word):
+    return word[-1] + word[1:-1] + word[0]
+
+def main():
+    test_cases = open(sys.argv[1], 'r')
+    for test in test_cases:
+        print ' '.join([swap_numbers(word) for word in test.strip().split(' ')])
+    test_cases.close()
+
+if __name__ == '__main__':
+    main()
