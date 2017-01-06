d02bcd31b1bd5af9ef64a028d2aa5db81b894635
Matt Pillar <matt@aerofs.com>
2017-01-06 11:41:09 -0800

    Solution to robo and robitta problem

--- 1-moderate/robo-and-robitta/main.py
+++ 1-moderate/robo-and-robitta/main.py
@@ -1,32 +1,14 @@
+import re
 import sys
 
 
-class GridSize(object):
-    def __init__(self, xy):
-        self.x, self.y = xy
+def walk(m, n, x, y):
+    c = 0
+    while n > y:
+        c, m, n, x, y = c + m, n - 1, m, n - y, x
+    return c + x
 
 
-class Point(object):
-    def __init__(self, xy):
-        self.x, self.y = xy
-
-
-def count_nuts(grid_size, destination):
-    #grid = [[0 for y in range(grid_size.y)] for x in range(grid_size.x)]
-    # TODO finish this.
-    return 0
-
-
-def main(input_filename):
-    test_cases = open(input_filename, 'r')
+with open(sys.argv[1], 'r') as test_cases:
     for test in test_cases:
-        test = test.strip()
-        grid_size = GridSize([int(x) for x in test.split('|')[0].strip().split('x')])
-        destination = Point([int(x) for x in test.split('|')[1].strip().split(' ')])
-        print(count_nuts(grid_size, destination))
-    test_cases.close()
-
-
-if __name__ == '__main__':
-    main(sys.argv[1])
-
+        print(walk(*map(int, re.findall(r'\d+', test))))
