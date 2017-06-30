c415271623e173afeee15ab99492a2ec7a694cf4
Matt Pillar <matt@aerofs.com>
2017-06-30 16:24:35 -0700

    Add solution to the consecutive primes problem

--- 1-moderate/consecutive-primes/main.cpp
+++ 1-moderate/consecutive-primes/main.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
+#include <set>
+#include <vector>
 #include <sstream>
 #include <fstream>
 
-unsigned int countPossibleNecklaceArrangements(unsigned int beads)
+std::set<unsigned int> primes({2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31});
+
+bool isPrime(unsigned int x)
 {
-    return 0;
+    return primes.find(x) != primes.end();
 }
 
+unsigned int countPossibleNecklaceArrangements(std::vector<unsigned int> &beads, unsigned int pos)
+{
+    if (beads.size() == pos) {
+        return isPrime(1 + beads[beads.size()-1]) ? 1 : 0;
+    }
+    unsigned int result = 0;
+    if (isPrime(beads[pos] + beads[pos-1])) {
+        result += countPossibleNecklaceArrangements(beads, pos+1);
+    }
+    for (unsigned int i = pos + 2; i < beads.size(); i += 2) {
+        if (isPrime(beads[pos-1] + beads[i]))  {
+            unsigned int tmp = beads[i];
+            beads[i] = beads[pos];
+            beads[pos] = tmp;
+
+            result += countPossibleNecklaceArrangements(beads, pos + 1);
+
+            tmp = beads[i];
+            beads[i] = beads[pos];
+            beads[pos] = tmp;
+        }
+    }
+    return result;
+}
 
 int main(int argc, char** argv)
 {
@@ -20,6 +48,11 @@ int main(int argc, char** argv)
         std::stringstream ss(line);
         ss >> x;
 
-        std::cout << countPossibleNecklaceArrangements(x) << std::endl;
+        std::vector<unsigned int> beads;
+        for (unsigned int i = 0; i < x; i++) {
+            beads.push_back(i+1);
+        }
+
+        std::cout << countPossibleNecklaceArrangements(beads, 1) << std::endl;
     }
 }
